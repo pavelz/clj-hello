9b82a8e6e334cfec50c44f8a37d89f007887b2f8
Pavel Zaitsev <pavel@arslogi.ca>
2024-03-05 11:54:00 -0500

    added a test

--- test/clj/core_test.clj
+++ test/clj/core_test.clj
@@ -7,3 +7,9 @@
     (println "woof")
     (is (= 1 1))))
 
+(deftest content-type-test
+    (testing "returns text/html when file name ends with .html"
+        (is (= "text/html" (content_type {:params {:name "index.html"}} "name"))))
+
+    (testing "returns binary/image when file name does not end with .html"
+        (is (= "binary/image" (content_type {:params {:name "image.png"}} "name")))))
