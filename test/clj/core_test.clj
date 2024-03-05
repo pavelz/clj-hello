8d73748514b95e0319e9f594a27af73021195086
Pavel Zaitsev <pavel@arslogi.ca>
2024-03-05 12:32:51 -0500

    remove dead code

--- test/clj/core_test.clj
+++ test/clj/core_test.clj
@@ -2,11 +2,6 @@
   (:require [clojure.test :refer :all]
             [clj.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (println "woof")
-    (is (= 1 1))))
-
 (deftest content-type-test
     (testing "returns text/html when file name ends with .html"
         (is (= "text/html" (content_type {:params {:name "index.html"}} "name"))))
